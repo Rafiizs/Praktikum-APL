e5851d25b73120eb861181e8a82d4e6b2ee6bb14
Rafiizs <123raffimhmd@gmail.com>
2025-03-08 21:52:47 +0800

    posttest1

---
+++ Post-test/Post-test-1/Praktek.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Library untuk input dan output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Mencetak teks ke layar
+    return 0;  // Mengembalikan nilai 0 menandakan program berhasil dijalankan
+}
