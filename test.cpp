ed190a027ab2d0f36c87204d0eec6986e4f6e795
Rafiizs <123raffimhmd@gmail.com>
2025-04-22 21:07:30 +0800

    post-test-5

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// void ceknilai (int *arr) {
+//     cout << "elemen pertama adalah: " 
+// }
+
+// int main () {
+//     string nama = "ade";
+//     cout << &nama << " adalah alamatnya " << nama << endl;
+//     return 0;
+// }
+
+// struct Address{
+//     string kota;
+//     string provinsi;
+//     string negara;
+// };
+//     int main(){
+//         // deklarasi variabel dengan struct
+//         Address address1, address2;
+//         // mengisi value address1
+//         address1.kota = "Samarinda";
+//         address1.provinsi = "Kalimantan Timur";
+//         address1.negara = "Indonesia";
+
+//         // mengcopy value address1 ke address2
+//         address2 = address1;
+
+//         // mengganti property kota dari samarinda ke ikn
+//         address2.kota = "ikn";
+
+//         // value address1
+//         cout<< address1.kota<<endl;
+//         // value address2
+//         cout<< address2.kota;
+//         return 0;
+// }
+
